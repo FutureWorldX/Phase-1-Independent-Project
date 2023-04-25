9f1df0c328d22f25e5db2e2dd9b0766555edcaba
Future <futureworldzone@gmail.com>
2023-04-25 15:36:03 +0300

    Loading existing HTML elements to the Index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 // JavaScript Code here
 
+//loading existing HTML elements to this JS file
+const clientName = document.querySelector("#clientName");
+const companyName = document.querySelector("#companyName");
+const clientEmail = document.querySelector("#email");
+const clientPhone = document.querySelector("#phone");
+const clientPoBox = document.querySelector("#po-box");
+const clientAddress = document.querySelector("#address");
+const clientCity = document.querySelector("#city");
+const clientCounty = document.querySelector("#county");
+const clientClaimReportDate = document.querySelector("#claimReportDate");
+const clientInsuranceType = document.querySelector("#insuranceType");
+const clientPolicyNum = document.querySelector("#policyNum");
+const clientPolicyStart = document.querySelector("#policyStart");
+const clientPolicyEnd = document.querySelector("#policyEnd");
+
 window.onload = () => {
     //intializing the ids and classes from the HTML page
     //navbar: class of ui centered grid
@@ -32,6 +47,39 @@ window.onload = () => {
                             policyList.appendChild(policyItem);
                            
                         }); //closing the forEach command when listing Policy classes
+
+                        //create a temporary table that fills in with data when submit button is clicked
+                        let tblClientDiv = document.createElement('div');
+                        let tblClient = document.createElement("table");
+
+                        //styling the div which holds the table
+                        tblClientDiv.style.display = "flex";
+                        tblClientDiv.style.margin = "0 auto";
+                        tblClientDiv.style.justifyContent = "center";
+                        tblClientDiv.style.alignItems = "center";
+                        tblClientDiv.style.textAlign = "center";
+                        tblClientDiv.style.overflow = "auto";
+
+                        //styling the table
+                        tblClient.style.fontFamily = "Arial, sans-serif";
+                        tblClient.style.backgroundColor = "mintcream";
+                        tblClient.style.padding = "10px";
+                        tblClient.style.border = "1px solid #ccc";
+                        tblClient.style.borderRadius = "5px";
+                        
+                        //appending the new div and table to the HTML document body
+                        tblClientDiv.appendChild(tblClient);
+                        document.body.appendChild(tblClientDiv);
+
+                        // Insert a row at the end of the table
+                        let newRow = tblClient.insertRow();
+
+                        // Insert a cell at the end of the row
+                        let newCell = newRow.insertCell();
+
+                        // Append a text node to the cell
+                        let newText = document.createTextNode('A new Text row has been created.');
+                        newCell.appendChild(newText);
 
                     }// closing when data.length > 0
                 else {
